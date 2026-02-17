1a18214cda7fc26b0c22ea024a833f0db89713b8
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2026-02-17 14:28:58 -0500

    can now read courses dir

--- advisor_engine.py
+++ advisor_engine.py
@@ -13,11 +13,38 @@ import ollama
 
 # Load degree requirements from JSON file
 
+
 def load_knowledge_base():
     """
     Load degree requirements and course data from our JSON file.
 
     Returns a dictonary with two keys:
-        - "degrees": 
+        - "degrees": full degree requirements
+        - "courses": the course catalog with prerequisites
     """
+
+    # Get the knowledge_base folder file path ready
+    ae_dir = os.path.abspath(__file__)
+    base_dir = os.path.dirname(ae_dir)
+    kb_dir = os.path.join(base_dir, "knowledge_base")
+
+    try: 
+        courses_dir = os.path.join(kb_dir, "courses.json")
+        with open(courses_dir, "r") as f:
+            courses = json.load(f)
+            print(courses)
+    except FileNotFoundError:
+        print(f"Error: The file {courses_dir} was not found")
+    except json.JSONDecodeError as e:
+        print("Invalid JSON syntax:", e)
+
+
+
+def main():
+    load_knowledge_base()
+
+
+if __name__ == "__main__":
+    main()
+
 

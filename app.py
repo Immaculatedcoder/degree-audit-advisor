4cecd565c808802d71561a1b81809ba21c7f522b
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2026-02-16 21:42:46 -0500

    Completed the degree.json

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-import streamlit as st
+# import streamlit as st
 
-st.write('Hello world!')
+print('Hello world!')
+# Day 2---
+# st.write('Hello world!')
+
+
+# Day 3---
+# st.header('st.button')
+
+# if st.button('Say hello'):
+#     st.write('Why hello there')
+# else:
+#     st.write('Goodbye')
+
+# Day ---
